b33d7ae5067d9848176d9987fd3395fad98ec6dc
ShreyTanna29 <shreytanna2957@gmail.com>
2024-12-18 05:16:36 +0530

    add bullets to shoot

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const ctx = canvas.getContext("2d");
 canvas.width = innerWidth;
 canvas.height = innerHeight;
 
+const bulletsArray = [];
+
+//classes
 class Player {
   constructor(x, y, radius, color) {
     this.x = x;
@@ -20,5 +23,56 @@ class Player {
   }
 }
 
+class Bullet {
+  constructor(x, y, radius, color, velocity) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.color = color;
+    this.velocity = velocity;
+  }
+
+  draw() {
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    ctx.fillStyle = this.color;
+    ctx.fill();
+  }
+
+  update() {
+    this.draw();
+    this.x += this.velocity.x;
+    this.y += this.velocity.y;
+  }
+}
+
+// invoking player class
 const player = new Player(canvas.width / 2, canvas.height / 2, 30, "red");
 player.draw();
+
+//animating bullets
+function animate() {
+  requestAnimationFrame(animate);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  player.draw();
+  bulletsArray.forEach((eachBullet) => eachBullet.update());
+}
+
+// event listeners
+addEventListener("click", (event) => {
+  const angle = Math.atan2(
+    event.clientY - canvas.height / 2,
+    event.clientX - canvas.width / 2
+  );
+
+  const velocity = {
+    x: Math.cos(angle),
+    y: Math.sin(angle),
+  };
+
+  bulletsArray.push(
+    new Bullet(canvas.width / 2, canvas.height / 2, 5, "pink", velocity)
+  );
+
+  animate();
+});
