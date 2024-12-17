290aef9112237e2fb0a18485ffe9b8a1b1f4a18b
ShreyTanna29 <shreytanna2957@gmail.com>
2024-12-18 03:41:04 +0530

    add player

---
+++ index.js
@@ -0,0 +1,24 @@
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+canvas.width = innerWidth;
+canvas.height = innerHeight;
+
+class Player {
+  constructor(x, y, radius, color) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.color = color;
+  }
+
+  draw() {
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    ctx.fillStyle = this.color;
+    ctx.fill();
+  }
+}
+
+const player = new Player(canvas.width / 2, canvas.height / 2, 30, "red");
+player.draw();
