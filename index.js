6e94d544abb5a4b45f85248c86c75bbc25e91c5f
ShreyTanna29 <shreytanna2957@gmail.com>
2024-12-18 22:58:36 +0530

    add enemies

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ canvas.width = innerWidth;
 canvas.height = innerHeight;
 
 const bulletsArray = [];
+const enemiesArray = [];
 
 //classes
 class Player {
@@ -41,24 +42,75 @@ class Bullet {
 
   update() {
     this.draw();
-    this.x += this.velocity.x;
-    this.y += this.velocity.y;
+    this.x += this.velocity.x * 10;
+    this.y += this.velocity.y * 10;
+  }
+}
+
+class Enemy {
+  constructor(x, y, radius, color, velocity) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.color = color;
+    this.velocity = velocity;
+  }
+
+  draw() {
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    ctx.fillStyle = this.color;
+    ctx.fill();
+  }
+
+  update() {
+    this.draw();
+    this.x += this.velocity.x * 5;
+    this.y += this.velocity.y * 5;
   }
 }
 
 // invoking player class
-const player = new Player(canvas.width / 2, canvas.height / 2, 30, "red");
+const player = new Player(canvas.width / 2, canvas.height / 2, 20, "red");
 player.draw();
 
-//animating bullets
+//generating enemies
+function spawnEnemies() {
+  setInterval(() => {
+    let radius = Math.random() * (50 - 10) + 10;
+    let x;
+    let y;
+    if (Math.random() < 0.5) {
+      x = Math.random() < 0.5 ? 0 - radius : canvas.width + radius;
+      y = Math.random() * canvas.height;
+    } else {
+      x = Math.random() * canvas.width;
+      y = Math.random() < 0.5 ? 0 - radius : canvas.height + radius;
+    }
+
+    let color = "green";
+    let angle = Math.atan2(canvas.height / 2 - y, canvas.width / 2 - x);
+
+    let velocity = {
+      x: Math.cos(angle),
+      y: Math.sin(angle),
+    };
+
+    enemiesArray.push(new Enemy(x, y, radius, color, velocity));
+  }, 1000);
+}
+
+spawnEnemies();
+//animating
 function animate() {
   requestAnimationFrame(animate);
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   player.draw();
   bulletsArray.forEach((eachBullet) => eachBullet.update());
+  enemiesArray.forEach((enemy) => enemy.update());
 }
 
-// event listeners
+// making bullets on click
 addEventListener("click", (event) => {
   const angle = Math.atan2(
     event.clientY - canvas.height / 2,
@@ -73,6 +125,6 @@ addEventListener("click", (event) => {
   bulletsArray.push(
     new Bullet(canvas.width / 2, canvas.height / 2, 5, "pink", velocity)
   );
-
-  animate();
 });
+
+animate();
