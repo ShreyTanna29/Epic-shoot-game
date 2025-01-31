8d1bcd1b49d3f1290719ccd809b605fc16fe0a7c
ShreyTanna29 <shreytanna2957@gmail.com>
2025-01-31 17:32:23 +0530

    add animation landing page

--- frontend/src/components/Landing.tsx
+++ frontend/src/components/Landing.tsx
@@ -1,25 +1,50 @@
 import { Link } from "react-router-dom"
 import gameImg from "../assets/game.webp"
 import logo from "../assets/logo.png"
+import { useEffect } from "react"
+import gsap from "gsap"
 
 export default function Landing() {
+
+    useEffect(() => {
+        gsap.to('.slideRight', {
+            opacity: 1,
+            x: 0,
+            duration: 1
+        })
+
+        gsap.to('.opacityAnimation5', {
+            opacity: 1,
+            duration: 5,
+        })
+        gsap.to('.opacityAnimation4', {
+            opacity: 0.2,
+            duration: 4,
+        })
+        gsap.to('.slideUp', {
+            opacity: 1,
+            duration: 1,
+            y: 0
+        })
+    }, [])
+
     return (
         <div className="w-full h-[100svh] relative">
-            <div className='w-full h-full absolute'>
-                <img src={gameImg} alt="Loading..." className="object-cover w-full h-full opacity-20 " />
+            <div className='w-full h-full absolute '>
+                <img src={gameImg} alt="Loading..." className="object-cover w-full h-full opacity-0  opacityAnimation4" />
             </div>
-            <div className=" flex p-3 items-center gap-2 relative ">
+            <div className=" flex p-3 items-center gap-2 relative -translate-x-10 opacity-0 slideRight ">
                 <div className="bg-blacks relative">
                     <img src={logo} alt="logo" width={50} height={50} className="opacity-90" />
                 </div>
                 <h3 className="text-white">Epic Shoot</h3>
             </div>
 
-            <div className="absolute top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%] text-white  " >
-                <h1 className="text-4xl font-extrabold ">A Neon Battlefield Where Only the Fastest Survive!</h1>
+            <div className="absolute top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%] text-white " >
+                <h1 className="text-4xl text-white font-extrabold opacity-0 opacityAnimation5">A Neon Battlefield Where Only the Fastest Survive!</h1>
                 <div className="flex justify-center">
                     <Link to={'/game'} >
-                        <button className="bg-white/10 mt-5 cursor-pointer hover:bg-white/40 py-2 px-12 rounded-lg  ">Play Now</button>
+                        <button className="bg-black/50 mt-5 cursor-pointer hover:bg-black py-2 px-12 rounded-lg opacity-0 translate-y-10 slideUp ">Play Now</button>
                     </Link>
 
                 </div>
